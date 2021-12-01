ea52f812e9de51cbd393530d39854771e59da0a4
pabmatale <pmateosalex@hotmail.com>
2021-12-01 16:15:04 +0100

    Configuracion Heroku2

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -180,3 +182,7 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+#COnfiguracion Heroku
+django_heroku.settings(locals())
+
